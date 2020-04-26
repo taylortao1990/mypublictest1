0414dbadcf3717e1f8c4c39a50edc5a8414dc2c0
taylor <408960505@qq.com>
2020-04-26 21:30:46 +0800

    commit一下试试看

---
+++ MyTest/src/com/smt/Test1.java
@@ -0,0 +1,13 @@
+package com.smt;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("SSSddddww");
+        System.out.println("王朝马汉");
+    }
+
+    public void fun1() {
+
+    }
+}
